f32f81cd0d21e1a73f219f9b3502c3ad7bae58d5
suhyun <suhyuni106@hanmail.net>
2022-01-11 10:20:54 +0900

    커밋테스트4

--- app/src/main/java/com/example/suhyun/cameracustomtest/TestActivity.java
+++ app/src/main/java/com/example/suhyun/cameracustomtest/TestActivity.java
@@ -39,6 +39,7 @@ public class TestActivity extends AppCompatActivity implements SurfaceHolder.Cal
 //        테스트주석2222
 
 //        테스트 주석 1111
+//        임시저장 - stash
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
